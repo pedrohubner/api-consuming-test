d67e0d3524a74cd1bf04164fcb9c338158cc36e2
pedrohubner <pedronhubner@gmail.com>
2020-09-03 11:51:14 -0300

    Implementando dependência de MockMvc com a anotação @MockBean e criando teste should_Return_GithubUserInfo_When_Find_byUserName()

---
+++ src/test/java/com/app/user/controllers/UserControllerTest.java
@@ -0,0 +1,27 @@
+package com.app.user.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void should_Return_GithubUserInfo_When_Find_byUserName() throws Exception {
+        mockMvc.perform(get("/users/{userName}", "pedrohubner"))
+                .andExpect(status().isOk());
+    }
+}
