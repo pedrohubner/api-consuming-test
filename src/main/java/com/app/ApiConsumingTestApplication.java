fb8c09d323368880d8831b506655e037e074065e
pedrohubner <pedronhubner@gmail.com>
2020-09-21 11:26:12 -0300

    Removendo anotações @RestController e @EnableEurekaClient

--- src/main/java/com/app/ApiConsumingTestApplication.java
+++ src/main/java/com/app/ApiConsumingTestApplication.java
@@ -2,20 +2,9 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@EnableEurekaClient
 @SpringBootApplication
-public class ApiConsumingTestApplication extends SpringBootServletInitializer {
-
-	@RequestMapping(value = "/")
-	public String home() {
-		return "Hello world";
-	}
+public class ApiConsumingTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiConsumingTestApplication.class, args);
